1d534204f4c1b6f7efdf344dca6266fbb394a904
Ethan Walker <inclushe@gmail.com>
2024-04-12 00:53:46 -0400

    Fixed logo not loading when offline

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		react(),
 		VitePWA({
 			registerType: "autoUpdate",
 			includeAssets: ["favicon.ico"],
@@ -30,7 +31,12 @@ export default defineConfig({
 				globPatterns: ["**/*.{svg}"],
 			},
 		}),
-		react(),
 	],
-	assetsInclude: ["**/*.csv"],
+	// add this to cache all the imports
+	workbox: {
+		globPatterns: ["**/*"],
+	},
+	// add this to cache all the
+	// static assets in the public folder
+	includeAssets: ["**/*"],
 });
